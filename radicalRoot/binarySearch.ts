a7ad4292db443859187536cd9e90084713b6adf9
7kama <neush0wch1ck@gmail.com>
2024-01-30 21:30:59 +0900

    Remove extra items

--- radicalRoot/binarySearch.ts
+++ radicalRoot/binarySearch.ts
@@ -1,13 +1,11 @@
 const binarySearch = (exponents: number, radicand: number, significantDigits: number): 
-{floatingRoot: number, loopTimes: number, calculateProcess: number[]} => {
+number => {
   let upper = radicand
   let lower = 0
   const tolerance = 0.1 ** (significantDigits + 1)
   let floatingRoot = 1
   let previousResult = 0
   const maxHandleTimes = 1000
-  let breakTimes = 0
-  const calculateProcess: number[] = []
   
   for (let i = 1; i <= maxHandleTimes; i++) {
     floatingRoot = (lower + upper) / 2
@@ -17,12 +15,9 @@ const binarySearch = (exponents: number, radicand: number, significantDigits: nu
       lower = floatingRoot
     }
     if (Math.abs(floatingRoot - previousResult) < tolerance) {
-      breakTimes = i - 1
       break
     }
-    if (i <= 10) calculateProcess.push(floatingRoot)
     previousResult = floatingRoot
   }
-  const loopTimes = breakTimes == 0 ? maxHandleTimes : breakTimes
-  return {floatingRoot, loopTimes, calculateProcess}
+  return floatingRoot
 }
--- radicalRoot/newtonsAlgorithm.ts
+++ radicalRoot/newtonsAlgorithm.ts
@@ -1,6 +1,5 @@
 const newtonsAlgorithm = (exponents: number, radicand: number, significantDigits: number): 
-{floatingRoot: number, loopTimes: number, calculateProcess: number[]} => {
-  const calculateProcess: Array<number> = []
+number => {
   const tolerance = 0.1 ** (significantDigits + 1)
   let floatingRoot = 1
   let previousResult = 0
@@ -15,19 +14,14 @@ const newtonsAlgorithm = (exponents: number, radicand: number, significantDigits
     return (radicand - intercept) / slope
   }
 
-  let breakTimes = 0
   const maxHandleTimes = 1000
   for (let i = 1; i <= maxHandleTimes; i++) {
     floatingRoot = newtonsHandle(floatingRoot)
-    if (Number.isNaN(floatingRoot)) new Error('計算に失敗しました。他のアルゴリズムを試してください。')
+    if (Number.isNaN(floatingRoot)) throw new Error('計算に失敗しました。他のアルゴリズムを試してください。')
     if (Math.abs(floatingRoot - previousResult) < tolerance) {
-      breakTimes = i - 1
       break
     }
-    if (i <= 10) calculateProcess.push(floatingRoot)
     previousResult = floatingRoot
   }
-
-  const loopTimes = breakTimes == 0 ? maxHandleTimes : breakTimes
-  return {floatingRoot, loopTimes, calculateProcess}
+  return floatingRoot
 }
